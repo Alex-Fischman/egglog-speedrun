cb3064acad03e28ba79254453d2530c654c74a9d
Alex Fischman <alexdfischman@gmail.com>
2023-08-12 13:49:18 -0700

    Rename parse.rs to syntax.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 // RangeInclusive doesn't work for `Token`s.
 #![allow(clippy::range_plus_one)]
 
-pub mod parse;
+pub mod syntax;
 
 fn main() {
     match run() {
@@ -19,9 +19,9 @@ fn run() -> Result<(), String> {
     let args = std::env::args().collect::<Vec<String>>();
     let name = args.get(1).ok_or("usage: pass an egglog file")?.clone();
     let text = std::fs::read_to_string(&name).map_err(|_| format!("could not read {name}"))?;
-    let source = std::rc::Rc::new(parse::Source { name, text });
+    let source = std::rc::Rc::new(syntax::Source { name, text });
 
-    let commands = parse::parse(&source)?;
+    let commands = syntax::parse(&source)?;
     for command in &commands {
         println!("{command}");
     }
