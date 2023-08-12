3b0c9807801b0d1b0e90f7792c7f5a9e284ed90c
Alex Fischman <alexdfischman@gmail.com>
2023-08-11 21:51:19 -0700

    Combine token.rs and parse.rs, add deny(missing_docs)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-mod parse;
-mod token;
+#![deny(missing_docs)]
+
+//! The command line interface for egglog.
+
+pub mod parse;
 
 fn main() {
     match run() {
@@ -12,16 +15,9 @@ fn run() -> Result<(), String> {
     let args = std::env::args().collect::<Vec<String>>();
     let name = args.get(1).ok_or("usage: pass an egglog file")?.to_owned();
     let text = std::fs::read_to_string(&name).map_err(|_| format!("could not read {}", name))?;
-    let source = std::rc::Rc::new(token::Source { name, text });
+    let source = std::rc::Rc::new(parse::Source { name, text });
 
-    let tokens = token::tokenize(&source);
-    println!("tokens:");
-    for token in &tokens {
-        println!("{}", token);
-    }
-    println!();
-
-    let parsed = parse::parse(&tokens)?;
+    let parsed = parse::parse(&source)?;
     println!("parsed:");
     for sexp in &parsed {
         println!("{}", sexp);
--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,52 @@
-use crate::token::*;
+//! This module parses `Source`s into `Sexp`s.
 
+pub use std::fmt::{Display, Formatter, Result as FmtResult};
+
+/// A `Source` represents a string to be parsed, as well as a name to be used in error messages.
+pub struct Source {
+    /// The name of this `Source`, to be used in error messages.
+    pub name: String,
+    /// The text of this `Source`, to be used in `Token`s.
+    pub text: String,
+}
+
+/// A slice of a `Source` string.
+pub struct Token<'a> {
+    /// The `Source` that this token comes from.
+    pub source: &'a Source,
+    range: std::ops::Range<usize>,
+}
+
+impl Token<'_> {
+    /// Get the string slice that this token holds.
+    pub fn as_str(&self) -> &str {
+        &self.source.text[self.range.clone()]
+    }
+}
+
+impl Display for Token<'_> {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        let mut row = 1;
+        let mut col = 1;
+        for (i, c) in self.source.text.char_indices() {
+            match (i, c) {
+                (i, _) if i == self.range.start => break,
+                (_, '\n') => {
+                    row += 1;
+                    col = 1;
+                }
+                _ => col += 1,
+            }
+        }
+        write!(f, "{} at {}:{row}:{col}", self.as_str(), self.source.name)
+    }
+}
+
+/// An unrestricted S-expression.
 pub enum Sexp<'a> {
-    Atom(&'a Token<'a>),
+    /// A single `Token`.
+    Atom(Token<'a>),
+    /// A list of S-expressions.
     List(Vec<Sexp<'a>>),
 }
 
@@ -23,34 +68,62 @@ impl Display for Sexp<'_> {
     }
 }
 
-pub fn parse<'a>(tokens: &'a [Token<'a>]) -> Result<Vec<Sexp<'a>>, String> {
-    // must not be empty
-    type Tokens<'a> = std::iter::Peekable<std::slice::Iter<'a, Token<'a>>>;
-    fn parse<'a>(tokens: &mut Tokens<'a>) -> Result<Sexp<'a>, String> {
-        let token = tokens.next().unwrap();
-        match token.as_str() {
-            ")" => Err(format!("extra {}", token)),
-            "(" => {
-                let mut list = vec![];
-                loop {
-                    match tokens.peek() {
-                        None => return Err(String::from("missing )")),
-                        Some(token) if token.as_str() == ")" => {
-                            tokens.next();
-                            return Ok(Sexp::List(list));
-                        }
-                        Some(_) => list.push(parse(tokens)?),
-                    }
-                }
-            }
-            _ => Ok(Sexp::Atom(token)),
+/// Parse a source string into S-expressions.
+pub fn parse(source: &Source) -> Result<Vec<Sexp>, String> {
+    // split source text into tokens
+    let mut tokens: Vec<(Token, bool)> = vec![];
+    for (i, c) in source.text.char_indices() {
+        match c {
+            '(' | ')' => tokens.push((
+                Token {
+                    source,
+                    range: i..i + 1,
+                },
+                false,
+            )),
+            c if c.is_whitespace() => {}
+            _ => match tokens.last_mut() {
+                Some((token, is_atom)) if token.range.end == i && *is_atom => token.range.end += 1,
+                _ => tokens.push((
+                    Token {
+                        source,
+                        range: i..i + 1,
+                    },
+                    true,
+                )),
+            },
         }
     }
 
+    // parse token stream into sexps
     let mut out = vec![];
-    let mut tokens = tokens.iter().peekable();
+    let mut tokens = tokens.into_iter().map(|(token, _)| token).peekable();
     while tokens.peek().is_some() {
-        out.push(parse(&mut tokens)?);
+        out.push(parse_sexp(&mut tokens)?);
     }
     Ok(out)
+}
+
+// `tokens` must not be empty
+fn parse_sexp<'a>(
+    tokens: &mut std::iter::Peekable<impl Iterator<Item = Token<'a>>>,
+) -> Result<Sexp<'a>, String> {
+    let token = tokens.next().unwrap();
+    match token.as_str() {
+        ")" => Err(format!("extra {}", token)),
+        "(" => {
+            let mut list = vec![];
+            loop {
+                match tokens.peek() {
+                    None => return Err(String::from("missing )")),
+                    Some(token) if token.as_str() == ")" => {
+                        tokens.next();
+                        return Ok(Sexp::List(list));
+                    }
+                    Some(_) => list.push(parse_sexp(tokens)?),
+                }
+            }
+        }
+        _ => Ok(Sexp::Atom(token)),
+    }
 }
