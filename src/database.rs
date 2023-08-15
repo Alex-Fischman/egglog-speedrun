b84fa2dc7b55a2c1f1b95568a9906946359dc1fe
Alex Fischman <alexdfischman@gmail.com>
2023-08-15 13:46:45 -0700

    Move State to database.rs and rename it to Database

---
+++ src/database.rs
@@ -0,0 +1,160 @@
+//! This module defines the state for the whole `egglog` program.
+
+use crate::*;
+
+/// The current state of the `egglog` program.
+#[derive(Default)]
+pub struct Database<'a> {
+    sorts: Vec<String>,
+    funcs: HashMap<String, Table>,
+    rules: Vec<(Vec<Pattern>, Vec<Action<'a>>)>,
+}
+
+impl Display for Database<'_> {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        for (f_, table) in &self.funcs {
+            for (xs, y) in table.rows() {
+                writeln!(
+                    f,
+                    "{f_}: {}: {y}",
+                    xs.iter()
+                        .map(|x| format!("{x}"))
+                        .collect::<Vec<_>>()
+                        .join(" ")
+                )?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Database<'a> {
+    /// Add a sort to this `Database`.
+    pub fn sort(&mut self, sort: String) -> Result<(), String> {
+        if self.sorts.contains(&sort) {
+            return Err(format!("{sort} was declared twice"));
+        }
+        self.sorts.push(sort);
+        Ok(())
+    }
+
+    /// Add a function to this `Database`.
+    pub fn function(
+        &mut self,
+        f: String,
+        xs: Vec<Type>,
+        y: Type,
+        merge: Option<Expr>,
+    ) -> Result<(), String> {
+        if self.funcs.contains_key(&f) {
+            return Err(format!("{f} was declared twice"));
+        }
+        self.funcs.insert(f, Table::new(xs, y, merge));
+        Ok(())
+    }
+
+    /// Add a rule to this `Database`.
+    pub fn rule(&mut self, patterns: Vec<Pattern>, actions: Vec<Action<'a>>) -> Result<(), String> {
+        self.rules.push((patterns, actions));
+        Ok(())
+    }
+
+    /// Get the value of `expr` given the functions in this `Database`.
+    pub fn check(&mut self, expr: &Expr) -> Result<Value, String> {
+        expr.evaluate(&HashMap::new(), &self.funcs)
+    }
+
+    /// Run an action to this `Database`.
+    pub fn action(&mut self, action: &Action) -> Result<bool, String> {
+        run_action(action, &HashMap::new(), &mut self.funcs)
+    }
+
+    /// Run the rules in this `Database` to fixpoint.
+    pub fn run(&mut self) -> Result<(), String> {
+        let mut changed = true;
+        while changed {
+            changed = false;
+            for (patterns, actions) in &self.rules {
+                // per pattern, per row, list of assignments
+                let binding: Vec<Vec<Vec<(&str, Value)>>> = patterns
+                    .iter()
+                    .map(|pattern| {
+                        self.funcs[&pattern.f]
+                            .rows()
+                            .map(|(xs, _)| {
+                                pattern
+                                    .xs
+                                    .iter()
+                                    .zip(xs)
+                                    .map(|(a, b)| (a.as_str(), *b))
+                                    .collect()
+                            })
+                            .collect()
+                    })
+                    .collect();
+                // per rows, per pattern, assignments
+                let bindings = multi_cartesian_product(binding);
+                // flatten, filter out non-matching assignments, convert to hashmaps
+                let bindings = bindings.into_iter().filter_map(|binding| {
+                    let mut out: HashMap<&str, Value> = HashMap::new();
+                    for assignments in binding {
+                        for (key, value) in assignments {
+                            if let Some(v) = out.get(key) {
+                                if *v != value {
+                                    return None;
+                                }
+                            } else {
+                                out.insert(key, value);
+                            }
+                        }
+                    }
+                    Some(out)
+                });
+
+                for binding in bindings {
+                    for action in actions {
+                        if run_action(action, &binding, &mut self.funcs)? {
+                            changed = true;
+                        }
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+fn run_action(
+    action: &Action,
+    vars: &HashMap<&str, Value>,
+    funcs: &mut HashMap<String, Table>,
+) -> Result<bool, String> {
+    match action {
+        Action::Insert(_, f, xs, y) => {
+            let xs = xs
+                .iter()
+                .map(|x| x.evaluate(vars, funcs))
+                .collect::<Result<Vec<_>, _>>()?;
+            let y = y.evaluate(vars, funcs)?;
+            let changed = funcs
+                .get_mut(f.as_str())
+                .ok_or(format!("unknown function {f}"))?
+                .insert(&xs, y)?;
+            Ok(changed)
+        }
+    }
+}
+
+fn multi_cartesian_product<T: Clone>(vecs: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    vecs.into_iter().fold(vec![vec![]], |xss, ys| {
+        let mut out = Vec::new();
+        for xs in xss {
+            for y in &ys {
+                let mut xs = xs.clone();
+                xs.push(y.clone());
+                out.push(xs);
+            }
+        }
+        out
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_panics_doc)]
 
+pub mod database;
 pub mod expr;
 pub mod syntax;
 pub mod table;
 
-pub use crate::{expr::*, syntax::*, table::*};
+pub use crate::{database::*, expr::*, syntax::*, table::*};
 pub use std::collections::HashMap;
 pub use std::fmt::{Display, Formatter, Result as FmtResult};
 
@@ -27,158 +28,18 @@ fn run() -> Result<(), String> {
     let source = std::rc::Rc::new(Source { name, text });
 
     let commands = parse(&source)?;
-    let mut state = State::new();
+    let mut database = Database::default();
     for command in commands {
-        state.run_command(command)?;
+        match command {
+            Command::Sort(_, sort) => database.sort(sort)?,
+            Command::Function(_, f, xs, y, merge) => database.function(f, xs, y, merge)?,
+            Command::Rule(_, patterns, actions) => database.rule(patterns, actions)?,
+            Command::Run(_) => database.run()?,
+            Command::Check(token, expr) => println!("{token}: {}", database.check(&expr)?),
+            Command::Action(action) => {
+                database.action(&action)?;
+            }
+        }
     }
     Ok(())
-}
-
-struct State<'a> {
-    sorts: Vec<String>,
-    funcs: HashMap<String, Table>,
-    rules: Vec<(Vec<Pattern>, Vec<Action<'a>>)>,
-}
-
-impl Display for State<'_> {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        for (f_, table) in &self.funcs {
-            for (xs, y) in table.rows() {
-                writeln!(
-                    f,
-                    "{f_}: {}: {y}",
-                    xs.iter()
-                        .map(|x| format!("{x}"))
-                        .collect::<Vec<_>>()
-                        .join(" ")
-                )?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<'a> State<'a> {
-    fn new() -> State<'a> {
-        State {
-            sorts: Vec::new(),
-            funcs: HashMap::new(),
-            rules: Vec::new(),
-        }
-    }
-
-    fn run_command(&mut self, command: Command<'a>) -> Result<(), String> {
-        let vars = HashMap::new();
-        match command {
-            Command::Sort(token, sort) => {
-                if self.sorts.contains(&sort) {
-                    return Err(format!("{sort} was declared before {token}"));
-                }
-                self.sorts.push(sort);
-            }
-            Command::Function(token, f, xs, y, merge) => {
-                if self.funcs.contains_key(&f) {
-                    return Err(format!("{f} was declared before {token}"));
-                }
-                self.funcs.insert(f, Table::new(xs, y, merge));
-            }
-            Command::Rule(_, patterns, actions) => self.rules.push((patterns, actions)),
-            Command::Run(_) => self.run_fixpoint()?,
-            Command::Check(token, expr) => {
-                println!("{token}: {}", expr.evaluate(&vars, &self.funcs)?);
-            }
-            Command::Action(action) => {
-                run_action(&action, &vars, &mut self.funcs)?;
-            }
-        }
-        Ok(())
-    }
-
-    fn run_fixpoint(&mut self) -> Result<(), String> {
-        let mut changed = true;
-        while changed {
-            changed = false;
-            for (patterns, actions) in &self.rules {
-                // per pattern, per row, list of assignments
-                let binding: Vec<Vec<Vec<(&str, Value)>>> = patterns
-                    .iter()
-                    .map(|pattern| {
-                        self.funcs[&pattern.f]
-                            .rows()
-                            .map(|(xs, _)| {
-                                pattern
-                                    .xs
-                                    .iter()
-                                    .zip(xs)
-                                    .map(|(a, b)| (a.as_str(), *b))
-                                    .collect()
-                            })
-                            .collect()
-                    })
-                    .collect();
-                // per rows, per pattern, assignments
-                let bindings = multi_cartesian_product(binding);
-                // flatten, filter out non-matching assignments, convert to hashmaps
-                let bindings = bindings.into_iter().filter_map(|binding| {
-                    let mut out: HashMap<&str, Value> = HashMap::new();
-                    for assignments in binding {
-                        for (key, value) in assignments {
-                            if let Some(v) = out.get(key) {
-                                if *v != value {
-                                    return None;
-                                }
-                            } else {
-                                out.insert(key, value);
-                            }
-                        }
-                    }
-                    Some(out)
-                });
-
-                for binding in bindings {
-                    for action in actions {
-                        if run_action(action, &binding, &mut self.funcs)? {
-                            changed = true;
-                        }
-                    }
-                }
-            }
-        }
-        Ok(())
-    }
-}
-
-fn run_action(
-    action: &Action,
-    vars: &HashMap<&str, Value>,
-    funcs: &mut HashMap<String, Table>,
-) -> Result<bool, String> {
-    match action {
-        Action::Insert(_, f, xs, y) => {
-            let xs = xs
-                .iter()
-                .map(|x| x.evaluate(vars, funcs))
-                .collect::<Result<Vec<_>, _>>()?;
-            let y = y.evaluate(vars, funcs)?;
-            let changed = funcs
-                .get_mut(f.as_str())
-                .ok_or(format!("unknown function {f}"))?
-                .insert(&xs, y)?;
-            Ok(changed)
-        }
-    }
-}
-
-fn multi_cartesian_product<T: Clone>(vecs: Vec<Vec<T>>) -> Vec<Vec<T>> {
-    vecs.into_iter().fold(vec![vec![]], |xss, ys| {
-        let mut out = Vec::new();
-        for xs in xss {
-            for y in &ys {
-                let mut xs = xs.clone();
-                xs.push(y.clone());
-                out.push(xs);
-            }
-        }
-        out
-    })
 }
