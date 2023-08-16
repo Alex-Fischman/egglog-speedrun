73164a901f908e75507d9d636d548645b8f6aa71
Alex Fischman <alexdfischman@gmail.com>
2023-08-16 14:08:51 -0700

    Implement union by depth optimization and cleanup

--- src/unionfind.rs
+++ src/unionfind.rs
@@ -5,13 +5,15 @@
 /// It also supports having each set map to a value, and merging them when their
 /// sets are unioned. If you don't want values, use () as the value and call `default`.
 pub struct UnionFind<'a, V> {
-    trees: Vec<ParentOrValue<V>>,
+    trees: Vec<Node<V>>,
     merge: Box<dyn Fn(V, V) -> Result<V, String> + 'a>,
 }
 
-enum ParentOrValue<V> {
-    Parent(usize),
-    Value(V),
+enum Node<V> {
+    // index of parent
+    Child(usize),
+    // value, rank
+    Root(V, usize),
 }
 
 impl Default for UnionFind<'static, ()> {
@@ -34,28 +36,28 @@ impl<'a, V> UnionFind<'a, V> {
 
     /// Create a new key with a given value and return it.
     pub fn new_key(&mut self, value: V) -> usize {
-        self.trees.push(ParentOrValue::Value(value));
+        self.trees.push(Node::Root(value, 0));
         self.trees.len() - 1
     }
 
     /// Get the canonical key associated with the given key.
     pub fn find(&mut self, key: usize) -> usize {
         match &self.trees[key] {
-            ParentOrValue::Value(_) => key,
-            ParentOrValue::Parent(parent) => {
-                let out = self.find(*parent);
-                self.trees[key] = ParentOrValue::Parent(out);
-                out
+            Node::Root(..) => key,
+            Node::Child(parent) => {
+                let root = self.find(*parent);
+                self.trees[key] = Node::Child(root);
+                root
             }
         }
     }
 
     /// Get the value associated with the given key.
     pub fn find_value(&mut self, key: usize) -> &V {
-        let key = self.find(key);
-        match &self.trees[key] {
-            ParentOrValue::Value(v) => v,
-            ParentOrValue::Parent(_) => unreachable!(), // key must be a root!
+        let root = self.find(key);
+        match &self.trees[root] {
+            Node::Root(v, _) => v,
+            Node::Child(_) => unreachable!(), // root must be a root!
         }
     }
 
@@ -66,14 +68,26 @@ impl<'a, V> UnionFind<'a, V> {
         if a != b {
             // We want to move x and y into self.merge, so we swap them with a default value.
             // This is okay since we're about to replace them. (Luckily we have a default!)
-            let x = std::mem::replace(&mut self.trees[a], ParentOrValue::Parent(0));
-            let y = std::mem::replace(&mut self.trees[b], ParentOrValue::Parent(0));
-            let z = match (x, y) {
-                (ParentOrValue::Value(x), ParentOrValue::Value(y)) => (self.merge)(x, y)?,
+            let x = std::mem::replace(&mut self.trees[a], Node::Child(0));
+            let y = std::mem::replace(&mut self.trees[b], Node::Child(0));
+            let (z, p, q) = match (x, y) {
+                (Node::Root(x, p), Node::Root(y, q)) => ((self.merge)(x, y)?, p, q),
                 _ => unreachable!(), // a and b are both roots!
             };
-            self.trees[a] = ParentOrValue::Value(z);
-            self.trees[b] = ParentOrValue::Parent(a);
+            match p.cmp(&q) {
+                std::cmp::Ordering::Less => {
+                    self.trees[a] = Node::Child(b);
+                    self.trees[b] = Node::Root(z, q);
+                }
+                std::cmp::Ordering::Greater => {
+                    self.trees[a] = Node::Root(z, p);
+                    self.trees[b] = Node::Child(a);
+                }
+                std::cmp::Ordering::Equal => {
+                    self.trees[a] = Node::Root(z, p + 1);
+                    self.trees[b] = Node::Child(a);
+                }
+            }
         }
         Ok(())
     }
@@ -81,21 +95,21 @@ impl<'a, V> UnionFind<'a, V> {
     /// Merge a new value into an existing set without creating a new key.
     pub fn merge(&mut self, key: usize, x: V) -> Result<(), String> {
         // See comments for `Database::union` on why we do it this way.
-        let key = self.find(key);
-        let y = std::mem::replace(&mut self.trees[key], ParentOrValue::Parent(0));
-        let z = match y {
-            ParentOrValue::Value(y) => (self.merge)(x, y)?,
-            ParentOrValue::Parent(_) => unreachable!(),
+        let root = self.find(key);
+        let y = std::mem::replace(&mut self.trees[root], Node::Child(0));
+        let (z, r) = match y {
+            Node::Root(y, r) => ((self.merge)(x, y)?, r),
+            Node::Child(_) => unreachable!(),
         };
-        self.trees[key] = ParentOrValue::Value(z);
+        self.trees[root] = Node::Root(z, r);
         Ok(())
     }
 
     /// Get all of the canonical keys and their values in this `UnionFind`.
     pub fn iter(&self) -> impl Iterator<Item = (usize, &V)> {
         self.trees.iter().enumerate().filter_map(|(k, v)| match v {
-            ParentOrValue::Parent(_) => None,
-            ParentOrValue::Value(v) => Some((k, v)),
+            Node::Child(_) => None,
+            Node::Root(v, _) => Some((k, v)),
         })
     }
 }
