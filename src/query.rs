80d105a1fbb951640066157f259e649a2147031a
Alex Fischman <alexdfischman@gmail.com>
2023-08-16 11:36:27 -0700

    Started dependency constraints

--- src/query.rs
+++ src/query.rs
@@ -16,49 +16,6 @@ struct EqClass {
 impl Query {
     /// Construct a new `Query` from a multi-pattern.
     pub fn new(funcs: &HashSet<&String>, patterns: &[Pattern]) -> Result<Query, String> {
-        fn eq_from_expr(
-            // The expression to transform into an `EqClass`.
-            expr: &Expr,
-            // The set of functions that exist.
-            funcs: &HashSet<&String>,
-            // A `UnionFind` to combine the `EqClass`es.
-            eqs: &mut UnionFind<EqClass>,
-            // A list of `eqs` keys that are all in the same row.
-            rows: &mut Vec<(String, Vec<usize>)>,
-        ) -> Result<usize, String> {
-            Ok(match expr {
-                Expr::Var(var) => eqs.new_key(EqClass {
-                    names: HashSet::from([var.clone()]),
-                    ..EqClass::default()
-                }),
-                Expr::Call(f, xs) if funcs.contains(f) => {
-                    let mut row = Vec::new();
-                    for (i, x) in xs.iter().enumerate() {
-                        let x = eq_from_expr(x, funcs, eqs, rows)?;
-                        eqs.merge(
-                            x,
-                            EqClass {
-                                columns: vec![(f.clone(), i)],
-                                ..EqClass::default()
-                            },
-                        )?;
-                        row.push(x);
-                    }
-                    let y = eqs.new_key(EqClass {
-                        columns: vec![(f.clone(), xs.len())],
-                        ..EqClass::default()
-                    });
-                    row.push(y);
-                    rows.push((f.clone(), row));
-                    y
-                }
-                _ => eqs.new_key(EqClass {
-                    exprs: vec![expr.clone()],
-                    ..EqClass::default()
-                }),
-            })
-        }
-
         let mut eqs: UnionFind<EqClass> = UnionFind::new(|mut a: EqClass, mut b| {
             Ok(EqClass {
                 names: {
@@ -82,35 +39,45 @@ impl Query {
         for pattern in patterns {
             assert!(!pattern.0.is_empty());
 
-            let a = eq_from_expr(&pattern.0[0], funcs, &mut eqs, &mut rows)?;
+            let a = eqs_from_expr(&pattern.0[0], funcs, &mut eqs, &mut rows)?;
             for expr in &pattern.0[1..] {
-                let b = eq_from_expr(expr, funcs, &mut eqs, &mut rows)?;
+                let b = eqs_from_expr(expr, funcs, &mut eqs, &mut rows)?;
                 eqs.union(a, b)?;
             }
         }
 
         // add constraints across patterns when names are the same
-        let mut names_to_keys: HashMap<String, usize> = HashMap::new();
-        let mut to_union = Vec::new();
+        let mut names_to_keys: HashMap<String, Vec<usize>> = HashMap::new();
+        let mut to_union: Vec<(usize, usize)> = Vec::new();
         for (key, value) in eqs.iter() {
             for name in &value.names {
-                names_to_keys
-                    .entry(name.clone())
-                    .and_modify(|old| {
-                        to_union.push((*old, key));
-                        *old = key;
-                    })
-                    .or_insert(key);
+                let vec = names_to_keys.entry(name.clone()).or_default();
+                if let Some(old) = vec.last() {
+                    to_union.push((*old, key));
+                }
+                vec.push(key);
             }
         }
         for (a, b) in to_union {
             eqs.union(a, b)?;
         }
+        let names_to_keys: HashMap<String, usize> = names_to_keys
+            .into_iter()
+            .map(|(k, v)| (k, eqs.find(v[0]).0))
+            .collect();
 
-        // todo: add dependency constraints based on exprs
+        // Dependency constraints, where each key is a canonical key in `eqs`, and
+        // the value is the set of canonical keys that any of its `exprs` depend on.
+        let mut deps = HashMap::new();
+        for (key, class) in eqs.iter() {
+            let mut set = HashSet::new();
+            for expr in &class.exprs {
+                deps_from_expr(expr, &names_to_keys, &mut set);
+            }
+            deps.insert(key, set);
+        }
 
-        let classes: Vec<_> = eqs.iter().map(|t| t.1).collect();
-        for class in classes {
+        for (_, class) in eqs.iter() {
             println!("names: {:?}", class.names);
             println!("exprs: {:?}", class.exprs);
             println!("columns: {:?}", class.columns);
@@ -167,6 +134,60 @@ impl Query {
         todo!("run query");
         [].into_iter()
     }
+}
+
+fn eqs_from_expr(
+    // The expression to transform into an `EqClass`.
+    expr: &Expr,
+    // The set of functions that exist.
+    funcs: &HashSet<&String>,
+    // A `UnionFind` to combine the `EqClass`es.
+    eqs: &mut UnionFind<EqClass>,
+    // A list of `eqs` keys that are all in the same row.
+    rows: &mut Vec<(String, Vec<usize>)>,
+) -> Result<usize, String> {
+    Ok(match expr {
+        Expr::Var(var) => eqs.new_key(EqClass {
+            names: HashSet::from([var.clone()]),
+            ..EqClass::default()
+        }),
+        Expr::Call(f, xs) if funcs.contains(f) => {
+            let mut row = Vec::new();
+            for (i, x) in xs.iter().enumerate() {
+                let x = eqs_from_expr(x, funcs, eqs, rows)?;
+                eqs.merge(
+                    x,
+                    EqClass {
+                        columns: vec![(f.clone(), i)],
+                        ..EqClass::default()
+                    },
+                )?;
+                row.push(x);
+            }
+            let y = eqs.new_key(EqClass {
+                columns: vec![(f.clone(), xs.len())],
+                ..EqClass::default()
+            });
+            row.push(y);
+            rows.push((f.clone(), row));
+            y
+        }
+        _ => eqs.new_key(EqClass {
+            exprs: vec![expr.clone()],
+            ..EqClass::default()
+        }),
+    })
+}
+
+fn deps_from_expr(
+    // The `Expr` to find the dependencies of.
+    _expr: &Expr,
+    // A map from variable names to canoncial keys.
+    _names: &HashMap<String, usize>,
+    // The set of dependencies.
+    _deps: &mut HashSet<usize>,
+) {
+    todo!()
 }
 
 // fn multi_cartesian_product<T: Clone>(vecs: Vec<Vec<T>>) -> Vec<Vec<T>> {
