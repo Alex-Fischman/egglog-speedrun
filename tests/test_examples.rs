f347d76be92fcf1808c0c54c326175566d417d42
Alex Fischman <alexdfischman@gmail.com>
2023-08-25 15:18:22 -0700

    Simple integration tests complete

--- tests/test_examples.rs
+++ tests/test_examples.rs
@@ -7,22 +7,35 @@ const EXAMPLES: &str = "examples";
 
 #[test]
 fn test_examples() {
-    let _command = std::process::Command::new(EGGLOG);
-
     for example in std::fs::read_dir(std::path::Path::new(EXAMPLES))
         .unwrap()
         .map(Result::unwrap)
-        .inspect(|entry| assert!(entry.file_type().unwrap().is_file()))
+        .inspect(|entry| {
+            assert!(
+                entry.file_type().unwrap().is_file(),
+                "no directories are allowed in {EXAMPLES}"
+            );
+        })
         .map(|entry| entry.path())
         .filter(|path| match path.extension() {
             Some(extension) if extension == "egg" => true,
             Some(extension) if extension == "out" => false,
-            _ => panic!("expected either .egg or .out, found {}", path.display()),
+            _ => panic!("unexpected file {}", path.display()),
         })
     {
+        let mut output = std::process::Command::new(EGGLOG)
+            .arg(&example)
+            .output()
+            .expect("could not run egglog");
+        let mut actual = output.stdout;
+        actual.append(&mut output.stderr);
+        let actual = String::from_utf8(actual).unwrap();
+
         let mut expected = example.clone();
         assert!(expected.set_extension("out"));
+        let expected = std::fs::read_to_string(&expected)
+            .unwrap_or_else(|_| panic!("could not read {}", expected.display()));
 
-        panic!("{example:?} {expected:?}");
+        assert_eq!(expected, actual);
     }
 }
